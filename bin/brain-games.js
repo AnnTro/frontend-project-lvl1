58b95ccf53f3424902eca363b374db92b5620df8
Anna Gollerbach <an.hollerbach@gmail.com>
2020-07-15 14:38:45 +0300

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/local/bin/env node
 
-console.log('Welcome to the Brain Games!')
+import brGmes from '../cli.js';
+
+brGmes();
---
+++ cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const brGmes = () => {
+	console.log(`Welcome to the Brain Games!`);
+
+	const name = readlineSync.question(`May I have your name? `);
+	console.log(`Hi, ${name}!`);
+}
+
+export default brGmes;
