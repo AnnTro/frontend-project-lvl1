3bb8ee9aa4c9c36a7b4662f0e04a96b6777987f1
Anna Gollerbach <an.hollerbach@gmail.com>
2020-10-07 13:46:58 +0300

    step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainGcdGame from '../src/brain-gcd.js';
+
+brainGcdGame();
---
+++ src/brain-gcd.js
@@ -0,0 +1,25 @@
+import gameProcees from './index.js';
+
+const description = 'Find the greatest common divisor of given numbers';
+
+const findNODfunction = (str) => {
+  const arr = str.split(' ');
+  let a = Math.abs(Number(arr[0]));
+  let b = Math.abs(Number(arr[1]));
+
+  if (b > a) { const temp = a; a = b; b = temp; }
+  while (true) {
+    if (b === 0) return a.toString();
+    a %= b;
+    if (a === 0) return b.toString();
+    b %= a;
+  }
+};
+
+const numGenerator = () => Math.floor(Math.random() * 100) + 1;
+
+const expression = () => `${numGenerator()} ${numGenerator()}`;
+
+const brainGcdGame = () => gameProcees(description, expression, findNODfunction);
+
+export default brainGcdGame;
